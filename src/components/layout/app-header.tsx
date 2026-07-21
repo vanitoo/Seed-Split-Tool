ae22119c267d2e5b10d47c59c067cda0a00c20c7
vanitoo <56897888+vanitoo@users.noreply.github.com>
2026-07-22 01:54:40 +0300

    chore: remove unused template app header

--- src/components/layout/app-header.tsx
+++
@@ -1,8 +0,0 @@
-export function AppHeader() {
-  return (
-    <header className="app-header">
-      <a href="#main" className="brand"><span className="brand-icon">BT</span><span>Browser Tool</span></a>
-      <div className="header-actions"><span className="local-label">🔒 Local only</span><a className="github-link" href="https://github.com/OWNER/REPOSITORY" target="_blank" rel="noreferrer">GitHub</a></div>
-    </header>
-  );
-}
